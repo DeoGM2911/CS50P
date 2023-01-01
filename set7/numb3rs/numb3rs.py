052a76b3a0a4d94c8703b04638373e3787b0c4a5
DeoGM2911 <dung20042911@gmail.com>
2023-01-01 16:45:48 +0700

    On branch problem-sets  Changes to be committed: 	new file:   set7/numb3rs/numb3rs.py 	new file:   set7/numb3rs/numb3rs_no_regex.py 	new file:   set7/numb3rs/test_numb3rs.py 	new file:   set7/numb3rs/test_numb3rs_no_regex.py 	new file:   set7/watch/watch.py

---
+++ set7/numb3rs/numb3rs.py
@@ -0,0 +1,31 @@
+import re
+
+
+def main():
+    print(validate(input("IPv4 Address: ")))
+
+
+def validate(ip: str) -> bool:
+    """
+    This function use regex and show whether a string is a valid IPv4 Address.
+    Args:
+        ip (str): an IPv4 Address if passed all test, else it isn't
+                HAS TO BE IN THE FORMAT "XXX.XXX.XXX.XXX", where XXX is an integer in th range [0,255]
+
+    Returns: bool:
+        True means proper IPv4 Address
+        False means inproper address
+    """
+    # EXPECT a format of A.B.C.D with A, B, C, and D are integers in the [0, 255] range.
+    ipv4 = re.search(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$", ip)
+    if ipv4 is None:
+        return False
+    nums = ipv4.group(0).split(".")
+    for num in nums:
+        if int(num) > 255 or int(num) < 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    main()
---
+++ set7/numb3rs/numb3rs_no_regex.py
@@ -0,0 +1,40 @@
+def main():
+    print(validate_no_regex(input("Enter: ")))
+
+
+def validate_no_regex(ip: str) -> bool:
+    """
+        This function have the exact same function as the one which uses regex.
+        However, this function don't use regex.
+    Args:
+        ip (str): an IPv4 Address if passed all test
+                else it isn't
+    Returns: bool
+        True means proper IPv4 Address
+        False means inproper address
+    """
+    # Just in case the user input leading or trailing whitespaces
+    _ip = ip.strip()
+    # Check whether there are 4 componets and 3 dots.
+    if ip.count(".") != 3:
+        return False
+    comps = ip.split('.')
+    # Check if the components of the IP Address are numbers.
+    try:
+        list_of_comp = list(map(int, comps))
+    except ValueError:
+        return False
+    # Check if there is any whitespace or escape sequence.
+    # This block is only needed if the user inputs anything different form numbers and dots.
+    for string, integer in zip(comps, list_of_comp):
+        if len(string) != len(str(integer)):
+            return False
+    # Check if the numbers are in the range [0,255].
+    for num in list_of_comp:
+        if (0 >= num) or (num >= 255):
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    main()
