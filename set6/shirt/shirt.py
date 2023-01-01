647aa7d96d9dba7ec384b132ca7ac86bcc704a91
DeoGM2911 <dung20042911@gmail.com>
2023-01-01 14:18:48 +0700

    On branch problem-sets
     Changes to be committed:
    	new file:   set6/shirt/after1.jpg
    	new file:   set6/shirt/after2.jpg
    	new file:   set6/shirt/after3.jpg
    	modified:   set6/shirt/shirt.py

--- set6/shirt/shirt.py
+++ set6/shirt/shirt.py
@@ -1,4 +1,4 @@
-import Image
+from PIL import Image, ImageOps
 import sys
 
 if len(sys.argv) < 3:
@@ -16,11 +16,17 @@ if (not io_file[0].endswith(".png")) and (not io_file[0].endswith(".jpeg")) and 
 elif io_file[1].split(".")[1] != io_file[0].split(".")[1]:
     sys.exit("Input and output have different extensions!")
 
+# The size of the CS50 shirt is 600x600 (w.h); The size of before images are all 1200x1600
+shirt = Image.open("shirt.png")
 try:
-    with open(io_file[0]) as ifile:
-        pass
+    with Image.open(io_file[0]) as i_image:
+        i_image = ImageOps.fit(image=i_image, size=(600, 600))
+        i_image.paste(shirt, shirt)
+        i_image.save(io_file[1])
 except FileNotFoundError:
     sys.exit("No such file found!")
-
-with open(io_file[1]) as ofile:
-    pass
+# Just in case
+except OSError:
+    sys.exit("Problm with input or output file!")
+except ValueError:
+    sys.exit("Could not determined output format!")
