f166bcdb673b723b4fa813f099c0d6667bc05edd
DeoGM2911 <dung20042911@gmail.com>
2023-01-02 19:23:52 +0700

    On branch problem-sets  Changes to be committed: 	modified:   set3/grocery.py 	modified:   set4/adieu.py

--- set3/grocery.py
+++ set3/grocery.py
@@ -1,21 +1,21 @@
 dict_of_grocery = dict()
 
-#generate the grocery list
+# generate the grocery list
 while True:
     stuff = input("Items: ")
     if len(stuff) == 0: 
         break #control-d 
     dict_of_grocery[stuff.upper()] = dict_of_grocery.get(stuff.upper(), 0) + 1
     
-#Add stuff into the grocery list
+# Add stuff into the grocery list
 list_of_grocery = list()
 for item in dict_of_grocery.keys():
     list_of_grocery.append(f'{item}')
 
-#sort the list
+# sort the list
 list_of_grocery.sort()
 
-#Add occurences
+# Add occurences
 for i in range(len(list_of_grocery)):
     list_of_grocery[i] = f'{dict_of_grocery[list_of_grocery[i]]} {list_of_grocery[i]}'
     print(list_of_grocery[i])
--- set4/adieu.py
+++ set4/adieu.py
@@ -3,6 +3,18 @@ p = inflect.engine()
 # To solve the problem we use the join() method
 
 
+def main():
+    """
+    Returns: the lyrics with the grammatically correct version of the list of names
+    """
+    # At least one name so no issue
+    list_of_name = insert_name()
+    # if len(list_of_name) < 1:
+    #   print("Say goodbye to your friends!")
+    lyrics = p.join(list_of_name)
+    print(f"Adieu, adieu, to {lyrics}")
+
+
 def insert_name():
     """
     Returns: a list of names inputted by the user
@@ -16,16 +28,6 @@ def insert_name():
     return list_name
 
 
-def main():
-    """
-    Returns: the lyrics with the grammatically correct version of the list of names
-    """
-    # At least one name so no issue
-    list_of_name = insert_name()
-    # if len(list_of_name) < 1:
-    #   print("Say goodbye to your friends!")
-    lyrics = p.join(list_of_name)
-    print(f"Adieu, adieu, to {lyrics}")
 
 
 if __name__ == "__main__":
