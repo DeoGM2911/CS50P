319ea8324fb631fe872f9b002c11a7595f4c074a
DeoGM2911 <dung20042911@gmail.com>
2023-01-04 22:13:11 +0700

    On branch problem-sets  Changes to be committed: 	modified:   set7/working/test_working.py 	modified:   set7/working/working.py

---
+++ set7/working/test_working.py
@@ -0,0 +1,23 @@
+from working import convert
+import pytest as pt
+
+
+def test_correct():
+    assert convert("9:34 AM to 3 PM") == "9:34 to 15:00"
+    assert convert("9 AM to 5:45 PM") == "9:00 to 17:45"
+    assert convert("9:34 AM to 3:45 PM") == "9:34 to 15:45"
+    assert convert("9 AM to 3 PM") == "9:00 to 15:00"
+
+
+def test_correct_1():
+    assert convert("9 PM to 1:30 AM") == "21:00 to 1:30"
+    assert convert("8:45 PM to 12:45 AM") == "20:45 to 00:45"
+    assert convert("8 PM to 12 AM") == "20:00 to 00:00"
+
+
+def test_wrong_format():
+    with pt.raises(ValueError):
+        convert("cat")
+        convert("9 AM - 12 PM")
+        convert("13 PM to 12 AM")
+        convert("12:60 AM to 5 AM")
--- set7/working/working.py
+++ set7/working/working.py
@@ -1,10 +1,9 @@
 # Accepted 12-hour format:
 # + 9:00 AM to 5:00 PM
 # + 9 AM to 5 PM
-# AM and PM are case-sensitive and 
-# there is no period and there is a space between them and the hour
+# AM and PM are case-sensitive and
+# there is no period and there is a space between them and the hour.
 import re
-import sys
 
 
 def main():
@@ -12,10 +11,39 @@ def main():
 
 
 def convert(inp: str):
-    start, end = re.search(r"^[0-9]+(:[0-5][0-9])? AM to [0-9]+(:[0-5][0-9])? PM$", inp.strip())
+    hour = re.search(r"(^[0-9]+(?::[0-5][0-9])? (?:AM|PM)) to ([0-9]+(?::[0-5][0-9])? (?:AM|PM)$)", inp.strip())
+    if hour is None:
+        raise ValueError
 
+    # Take out the starting hour and the ending hour.
+    for hr in [hour.group(1), hour.group(2)]:
+        # Check for valid hour
+        if (int(hr.split(" ")[0].split(":")[0]) > 12) or (int(hr.split(" ")[0].split(":")[0]) <= 0):
+            raise ValueError
 
+    start = re.sub(r" (?:AM|PM)", "", hour.group(1))
+    end = re.sub(r" (?:AM|PM)", "", hour.group(2))
 
+    if hour.group(1).split(" ")[1] == "PM" and (hour.group(1).split(" ")[0].split(":")[0] != "12"):
+        # for convert PM to 24-hour format, we add 12 to hour.
+        start = re.sub(r"[0-9]+", str(int(start.split(":")[0]) + 12), start, count=1)
+    if hour.group(2).split(" ")[1] == "PM" and (hour.group(2).split(" ")[0].split(":")[0] != "12"):
+        # for convert PM to 24-hour format, we add 12 to hour.
+        end = re.sub(r"[0-9]+", str(int(end.split(":")[0]) + 12), end, count=1)
+
+    # Reformat if the user's input doesn't have the minute part (E.g 9 AM -> 9:00).
+    if ":" not in start:
+        start = f"{start}:00"
+    if ":" not in end:
+        end = f"{end}:00"
+
+    # Convert 12:xx AM to 00:xx.
+    if (hour.group(1).split(" ")[0].split(":")[0] == "12") and (hour.group(1).split(" ")[1] == "AM"):
+        start = f"00:{start.split(':')[1]}"
+    if (hour.group(2).split(" ")[0].split(":")[0] == "12") and (hour.group(2).split(" ")[1] == "AM"):
+        end = f"00:{end.split(':')[1]}"
+
+    return f"{start} to {end}"
 
 
 if __name__ == "__main__":
