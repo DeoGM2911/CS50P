8b3efdb984cf516fbc3d7489280b3753977ca40f
DeoGM2911 <dung20042911@gmail.com>
2023-01-06 20:45:47 +0700

    Add a comment  Changes to be committed: 	modified:   set7/response/response.py

--- set7/response/response.py
+++ set7/response/response.py
@@ -5,3 +5,9 @@ if checkers.is_email(inp_email):
     print("Valid")
 else:
     print("Invalid")
+
+# Just a note. The regex of email is:
+# ^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+ -> username
+# @[a-zA-Z0-9] -> @ and domain
+# (?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9]) -> additional info about domain
+# (?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$
