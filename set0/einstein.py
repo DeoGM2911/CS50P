52ede2aec85da872e2e8a2b4035672a609f49f57
DeoGM2911 <dung20042911@gmail.com>
2023-01-10 21:42:50 +0700

    modified:   set0/einstein.py

--- set0/einstein.py
+++ set0/einstein.py
@@ -1,2 +1,2 @@
-# Assume that the user will always input an int or a floating point
-print(float(input('Enter the mass: '))*(9*10**16))
+# Assume that the user will always input an int
+print(int(input('Enter the mass: '))*(9*(10**16)))
