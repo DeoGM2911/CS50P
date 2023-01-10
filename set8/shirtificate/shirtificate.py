18b3bb7d34de840b89a439aab950d6827fa1e5dc
DeoGM2911 <dung20042911@gmail.com>
2023-01-10 10:10:53 +0700

    Add new files on shirtificate problem  Changes to be committed: 	new file:   set8/shirtificate/shirtificate.png 	new file:   set8/shirtificate/shirtificate.py

---
+++ set8/shirtificate/shirtificate.py
@@ -0,0 +1,5 @@
+from fpdf import FPDF
+# pdf = FPDF(orientation="P", unit="mm", format="A4") : this code is for setting the orientation and format
+
+text = f"{input('What is your name? \n')} took CS50"
+
