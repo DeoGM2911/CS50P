c54019882e1ef06c0af9feab11d1d4f464575ec0
DeoGM2911 <dung20042911@gmail.com>
2023-01-10 09:57:47 +0700

    Modified plates.py and add plates_regex.py  Changes to be committed: 	modified:   set2/plates.py 	modified:   set2/plates_regex.py

--- set2/plates.py
+++ set2/plates.py
@@ -1,9 +1,11 @@
 def main():
-    plate = input("Plate: ").upper()
-    if is_valid(plate):
-        print("Valid")
-    else:
-        print("Invalid")
+    while True:
+        plate = input("Plate: ").upper()
+        if plate == "": break
+        if is_valid(plate):
+            print("Valid")
+        else:
+            print("Invalid")
 
 
 def is_valid(s: str):
@@ -16,61 +18,56 @@ def is_valid(s: str):
     # requirement No.1: 2<= length <=6
     if not 2 <= len(s) <= 6:
         return False
-
-    # requirement No.2: start with 2 letters
-    try:
-        if int(s[0]) in numbers:
-            return False
-        elif int(s[1]) in numbers:
-            return False
-        else:
-            if len(s) == 2:
-                return True
-    except ValueError:
-        pass   
-    # Check the last number of requirement 3
-    if not s[-1] in numbers:
-        return False
-
     # requirement No.4: no special character
     for i in range(2 - len(s), 0):
         if s[i] in special_char:
             return False
 
+    # requirement No.2: start with 2 letters
+    for letter in s[:2]:
+        if letter in numbers:
+            return False
+    if len(s) == 2:  # If the length of the plate is only 2, then it is valid
+        return True
+    
+    # Check the last number of requirement 3
+    if not s[-1] in numbers:
+        return False
+    if len(s) == 3 and s[-1] != "0": # If the length of the plate is 3, then it is valid
+        return True
+
+
     # requirement No.3: letter can't be the end of the plate and number can't be in the middle
     # for all other characters. The first two chars 've already satisfied the conditions.
-    if len(s) == 3:
+    # for length of 4, 5, 6
+    if not s[-2] in numbers:  # The case when the char right before the end is a letter
         if s[-1] == '0':
             return False
-    else:
-        if not s[-2] in numbers:  # The case when the char right before the end is a letter
-            if s[-1] == '0':
+        elif len(s) == 5 and s[-3] in numbers:
+            return False
+        elif len(s) == 6:
+            if s[-3] in numbers:
                 return False
-            elif len(s) == 5 and s[-3] in numbers:
+            else:
+                if s[-4] in numbers:
+                    return False
+    else:  # The case when the char right before the end is a number
+        if s[-2] == 0 and len(s) == 4:
+            return False  # if the length of the string is 4
+        elif len(s) == 5:  # if the length of the string is 5
+            if s[-3] == '0':
                 return False
-            elif len(s) == 6:
-                if s[-3] in numbers:
-                    return False
-                else:
-                    if s[-4] in numbers:
-                        return False
-        else:  # The case when the char right before the end is a number
-            if s[-2] == 0 and len(s) == 4:
-                return False  # if the length of the string is 4
-            elif len(s) == 5:  # if the length of the string is 5
-                if s[-3] == '0':
-                    return False
-                elif (not s[-3] in numbers) and (s[-2] == '0'):
-                    return False
-            elif len(s) == 6:  # if the length of the string is 6
-                if (not s[-3] in numbers) and (not s[-4] in numbers) and (s[-2] == '0'):
-                    return False
-                elif (s[-3] == '0') and (not s[-4] in numbers):
-                    return False
-                elif (s[-4] in numbers) and (not s[-3] in numbers):
-                    return False
-                elif (s[-3] in numbers) and s[-4] == '0':
-                    return False
+            elif (not s[-3] in numbers) and (s[-2] == '0'):
+                return False
+        elif len(s) == 6:  # if the length of the string is 6
+            if (not s[-3] in numbers) and (not s[-4] in numbers) and (s[-2] == '0'):
+                return False
+            elif (s[-3] == '0') and (not s[-4] in numbers):
+                return False
+            elif (s[-4] in numbers) and (not s[-3] in numbers):
+                return False
+            elif (s[-3] in numbers) and s[-4] == '0':
+                return False
     return True
 
 
--- set2/plates_regex.py
+++ set2/plates_regex.py
@@ -1,17 +1,42 @@
 # This program attempt to solve the problem "Vanity Plate" by using regular expression
+# The biggest different between using regex and normal conditions is that regex can provide the correct patterns,
+# while the conditions approach check all the wrong conditions
 import re
 
-plate = input("Plate: ")
 
-# The requirement of the number of characters between 2 and 6 is statisfied
-# The requirement that the first two characters are letters
-# The re.IGNORECASE covers the issue of case-insensitivity
-def is_valid(plate: str) -> bool:
-    if plate_el := re.match(r"[a-z]{2}([a-z0-9]{0,4}$)", plate, flags=re.IGNORECASE):
-        if len(plate_el.group(1)) < 1:  # If no additional character
-            return True
+def main():
+    while True:
+        plate = input("Plate: ")
+        if len(plate) < 1: break
+        if is_valid(plate):
+            print("Valid")
         else:
-            pass
-    
-    else:
+            print("Invalid")
+
+
+# The requirement of the number of characters between 2 and 6 is statisfied by the regex
+# The requirement that the first two characters are letters is statisfied by the regex
+# The re.IGNORECASE covers the issue of case-insensitivity
+# The regex of valid plates are:
+# * ^[a-z]{2}(?:[1-9][0-9]{3})?$
+# * ^[a-z]{2,5}[1-9]$
+# * ^[a-z]{2,4}[1-9][0-9]$
+# * ^[a-z]{2,3}[1-9][0-9]{2}$
+# We can use re.fullmatch() to eliminate ^ and $
+
+
+def is_valid(plate: str) -> bool:
+    if re.fullmatch(r"[a-z]{2}(?:[1-9][0-9]{3})?", plate, flags=re.IGNORECASE):
+        return True
+    elif re.fullmatch(r"[a-z]{2,5}[1-9]", plate, flags=re.IGNORECASE):
+        return True
+    elif re.fullmatch(r"[a-z]{2,4}[1-9][0-9]", plate, flags=re.IGNORECASE):
+        return True
+    elif re.fullmatch(r"[a-z]{2,3}[1-9][0-9]{2}", plate, flags=re.IGNORECASE):
+        return True
+    else: 
         return False
+
+
+if __name__ == "__main__":
+    main()
