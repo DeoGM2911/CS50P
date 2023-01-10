6deba0a1b631038e35727a784e9ee326b371f7db
DeoGM2911 <dung20042911@gmail.com>
2023-01-10 19:48:54 +0700

    Last problem in set 8  Changes to be committed: 	modified:   set8/shirtificate/result.pdf 	modified:   set8/shirtificate/shirtificate.py

--- set8/shirtificate/shirtificate.py
+++ set8/shirtificate/shirtificate.py
@@ -16,9 +16,9 @@ class PDF(FPDF):
         self.set_text_color(0, 0, 0)  # black
         # Printing title:
         self.cell(
-            width,
-            30,
-            self.title,
+            w=width,
+            h=45,
+            txt=self.title,
             border=0,
             new_x="LMARGIN",
             new_y="NEXT",
@@ -32,28 +32,38 @@ cert = f"{input('Your name: ')} took CS50"
 # Set the pdf's properties
 pdf = PDF()
 pdf.set_title("CS50 Shirtificate")
+# Add a page which automatically add the header
 pdf.add_page("P", "a4")  # Default is "P" and "a4"
 
 # Print the image
 pdf.image(
     "E:\CS50P\set8\shirtificate\shirtificate.png",
-    5,                # Centre horizontally (image's width + 2x == 210)
-    (297 - 220) / 2,  # Centre vertically (image's height + 2y == 297)
+    x=5,                # Centre horizontally (image's width + 2x == 210)
+    y=(297 - 210) / 2,  # Centre vertically (image's height + 2y == 297)
     w=200,            # Reshape the image's width
-    h=220             # Reshape the image's height
+    h=210             # Reshape the image's height
     ) 
 
-# Set font:
-pdf.set_font("Helvetica", "B", 20)
-width = pdf.get_string_width(cert)
+# The part below is to generate the text on the shirt
+# Set font and a new title:
+pdf.set_title(cert)
+pdf.set_font("Helvetica", "B", 32)
+
+# Set color for the text
+pdf.set_text_color(255, 255, 255)
+
+# Calculating width of text and setting cursor position:
+pdf.set_y((297 / 2) - (210 / 4)) # Put the cursor vertically so that it lands on top of the shirt
+width = pdf.get_string_width(cert) + 6
+pdf.set_x((210 - width) / 2)  # Change the x-coordinate of the cursor so that the text when printed will be centered
 
 # Print text:
 pdf.cell(
     w=width,
-    h=20,
+    h=32,
     txt=cert,
     border=0,
-    new_x="C",
+    new_x="LMARGIN",
     new_y="NEXT",
     align="C",
     fill=False
