96d2e0a6cc9f3cedf28cf3fc2f6f267889d62f8c
DeoGM2911 <dung20042911@gmail.com>
2023-01-14 11:33:27 +0700

    Change some tests based on new working.py Changes to be committed: 	modified:   set7/working/test_working.py

--- set7/working/test_working.py
+++ set7/working/test_working.py
@@ -3,15 +3,15 @@ import pytest as pt
 
 
 def test_correct():
-    assert convert("9:34 AM to 3 PM") == "9:34 to 15:00"
-    assert convert("9 AM to 5:45 PM") == "9:00 to 17:45"
-    assert convert("9:34 AM to 3:45 PM") == "9:34 to 15:45"
-    assert convert("9 AM to 3 PM") == "9:00 to 15:00"
+    assert convert("9:34 AM to 3 PM") == "09:34 to 15:00"
+    assert convert("9 AM to 5:45 PM") == "09:00 to 17:45"
+    assert convert("9:34 AM to 3:45 PM") == "09:34 to 15:45"
+    assert convert("9 AM to 3 PM") == "09:00 to 15:00"
     # 3 out of 4 assertions will fail if we don't account for the 9 AM to 5PM format and change it to 9:00 and 17:00
 
 
 def test_correct_1():
-    assert convert("9 PM to 1:30 AM") == "21:00 to 1:30"
+    assert convert("9 PM to 1:30 AM") == "21:00 to 01:30"
     assert convert("8:45 PM to 12:45 AM") == "20:45 to 00:45"
     assert convert("8 PM to 12 AM") == "20:00 to 00:00"
     # Two last assertions will fail if we don't convert 12:xx AM to 00:xx.
