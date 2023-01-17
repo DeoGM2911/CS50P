c998e21c370b9a538f1247f6699b5d0f2cb1aa40
DeoGM2911 <dung20042911@gmail.com>
2023-01-17 08:40:32 +0700

    modified:   final project/project.py

--- final project/project.py
+++ final project/project.py
@@ -2,6 +2,7 @@ import re
 from datetime import date
 from vehicle import Car, Motorbike
 import sys
+import argparse
 
 
 def main():
@@ -17,5 +18,18 @@ def check_age(dob):
     pass
 
 
-def regis_fee():
-    pass
+def regis_fee(vehicle, prc, city):
+    """Get the total price include tax of the vehicle
+    
+    Args:
+        vehicle (str): the type of vehicle (car or motorbike) 
+        prc (float): the price of the vehicle (tax not included) 
+        city (str): the name of the city where the vehicle is bought 
+
+    Returns:
+        str: the formated string that show the total price of the vehicle
+    """
+    if vehicle.strip().lower() == "car":
+        return Car(0, 0, prc, city=city).get_tot_price_car()  # Only the price and city are needed so other params are 0
+    if vehicle.strip().lower() == "motorbike":
+        return Motorbike(0, 0, prc).get_tot_price_motorbike()  # Only the price is needed so other params are 0
