6a55496da4b7ceed30dca8ee3ceca36d941e7808
DeoGM2911 <dung20042911@gmail.com>
2023-01-21 22:00:48 +0700

    set up the window of the app Changes to be committed: modified:   project/UI.py

--- project/UI.py
+++ project/UI.py
@@ -5,7 +5,15 @@ class UI:
     def __init__(self, master: ctk.CTk):
         self.master = master
         self.master.geometry("750x500")
-
+        self.master.resizable(False, False)
+        self.master.wm_title("Vehicle")
+        self.master.tk_setPalette("black")
+        
+        # Create the label for prompting
+        self.label = ctk.CTkLabel(self.master, text="Name", font=("Helvetica bold", 26))
+        self.label.place(relx=0.5, rely=0.25, anchor="center")
+        self.label = ctk.CTkLabel(self.master, text="Date of birth", font=("Helvetica bold", 26))
+        self.label.place(relx=0.5, rely=0.55, anchor="center")
 
 app = ctk.CTk()
 gui = UI(master=app)
